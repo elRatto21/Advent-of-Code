85556dc7b33be89589efde5c2e9b723db9516c60
Niklas Trapp <niklas031712@gmail.com>
2023-12-01 07:18:40 +0100

    remove old code

--- src/tag01/Aufgabe01.java
+++ src/tag01/Aufgabe01.java
@@ -1,35 +1,9 @@
 package tag01;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Aufgabe01 {
 
-	public static void main(String[] args) throws FileNotFoundException{
-		System.out.println(returnMaxCalories());
-	}
-	
-	public static int returnMaxCalories() throws FileNotFoundException {
-		File f = new File("./inputs/inputTag01Aufgabe01.txt");
-		Scanner sc = new Scanner(f);
-		String str;
-		int maxCalories = -1;
-		int elf = 0;
+	public static void main(String[] args) {
 		
-		while(sc.hasNextLine()) {
-			str = sc.nextLine();
-			if(!str.equalsIgnoreCase("")) {
-				elf += Integer.valueOf(str);
-			} else {
-				if(elf > maxCalories) {
-					maxCalories = elf;
-				}
-				elf = 0;
-			}
-		}
-		
-		sc.close();
-		return maxCalories;
 	}
 
 }
